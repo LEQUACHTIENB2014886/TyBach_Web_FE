4351f275d0c294b661b12488389eb24eedfd3dc3
LEQUACHTIENB2014886 <lequachtien123@gmail.com>
2025-02-04 10:43:51 +0700

    rrr

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,50 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ loading: fullscreenLoading }">
     <Header @update-language="updateLanguage" />
     <Headertitle @update-language="updateLanguage" />
-    <router-view></router-view>
+    <router-view />
     <Footer />
+    <el-button
+      v-loading.fullscreen.lock="fullscreenLoading"
+      type="primary"
+      @click="openFullScreen1"
+    >
+      As a directive
+    </el-button>
+    <el-button type="primary" @click="openFullScreen2">As a service</el-button>
   </div>
 </template>
 
 <script setup>
-import { computed, watch } from "vue";
+import { ref, onMounted, watch, computed } from "vue";
 import { useStore } from "vuex";
+import { ElLoading } from "element-plus";
 import Header from "./components/Header.vue";
 import Headertitle from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 
 const store = useStore();
 const locale = computed(() => store.getters["language/locale"]);
+
+const fullscreenLoading = ref(false);
+
+const openFullScreen1 = () => {
+  fullscreenLoading.value = true;
+  setTimeout(() => {
+    fullscreenLoading.value = false;
+  }, 2000);
+};
+
+const openFullScreen2 = () => {
+  const loading = ElLoading.service({
+    lock: true,
+    text: "Loading",
+    background: "rgba(0, 0, 0, 0.7)",
+  });
+  setTimeout(() => {
+    loading.close();
+  }, 2000);
+};
 
 const updateLanguage = (newLang) => {
   store.dispatch("language/changeLanguage", newLang);
@@ -26,8 +55,16 @@ const pageTitles = {
   en: "Home",
   zh: "首页",
 };
+
 watch(locale, (newLocale) => {
   document.title = pageTitles[newLocale] || "Trang chủ";
+});
+
+onMounted(() => {
+  fullscreenLoading.value = true;
+  setTimeout(() => {
+    fullscreenLoading.value = false;
+  }, 500);
 });
 </script>
 
@@ -35,6 +72,15 @@ watch(locale, (newLocale) => {
 #app {
   display: flex;
   flex-direction: column;
+  transition: opacity 0.5s ease;
+  opacity: 0;
 }
 
+#app.loading {
+  opacity: 0;
+}
+
+#app:not(.loading) {
+  opacity: 1;
+}
 </style>
